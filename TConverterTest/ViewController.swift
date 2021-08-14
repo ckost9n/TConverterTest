00a31f0d3b04cbb282c46ee0823c7ff13d6defb0
ckost9n <ckost9n@gmail.com>
2021-08-14 21:47:52 +0300

    Initial Commit

---
+++ TConverterTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TConverterTest
+//
+//  Created by Konstantin on 14.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
