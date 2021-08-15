735febc413cd942d0d8c3b73e4bd9e304111e749
ckost9n <ckost9n@gmail.com>
2021-08-15 18:14:17 +0300

    Complete

--- TConverterTest/ViewController.swift
+++ TConverterTest/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var celiusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = -100
+            slider.value = 0
+        }
+    }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        let temperatureCelsius = Int(round(sender.value))
+        celiusLabel.text = "\(temperatureCelsius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)ºF"
+        
     }
     
 }
